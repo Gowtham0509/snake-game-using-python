845e09dd72f2c342f27d28322f2e8b837051b1dc
Gowtham0509 <gowthamanandaraj02@gmail.com>
2026-05-31 14:34:55 +0530

    feature: added extension of snake

--- main.py
+++ main.py
@@ -30,11 +30,12 @@ while is_game_on:
     #Detect collision with food.
     if snake.head.distance(food) < 13:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     #Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         is_game_on = False
         scoreboard.game_over()
-        
+
 screen.exitonclick()
